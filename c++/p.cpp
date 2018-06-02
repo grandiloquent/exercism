262b6be450cb17438f9bb16724b2a146039b9f3a
grandiloquent <1785907865@qq.com>
2018-06-02 09:56:44 +0800

    changed

---
+++ c++/p.cpp
@@ -0,0 +1,251 @@
+#include <windows.h>
+#include <iostream>
+#include <Winuser.h>
+
+void Curve()
+{
+    int x = 388;
+    int y = 15;
+    SetCursorPos(x, y);
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 466;
+    y = 139;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1343;
+    y = 191;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1400;
+    y = 243;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1554;
+    y = 311;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1434;
+    y = 380;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+}
+void Curve1()
+{
+    int x = 388;
+    int y = 15;
+    SetCursorPos(x, y);
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 466;
+    y = 139;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1343;
+    y = 191;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1400;
+    y = 243;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1554;
+    y = 311;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1406;
+    y = 348;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+}
+void Curve2()
+{
+    int x = 388;
+    int y = 15;
+    SetCursorPos(x, y);
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 466;
+    y = 139;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1343;
+    y = 191;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1400;
+    y = 243;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1554;
+    y = 311;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1405;
+    y = 367;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+}
+void SaveAndClose()
+{
+    int x = 57;
+    int y = 8;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+    x = 91;
+    y = 173;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+    x = 718;
+    y = 331;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+    x = 965;
+    y = 139;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+}
+void Camera()
+{
+    int x = 388;
+    int y = 15;
+    SetCursorPos(x, y);
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 466;
+    y = 139;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1562;
+    y = 315;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    x = 1471;
+    y = 348;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+    x = 1552;
+    y = 652;
+    SetCursorPos(x, y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+    Sleep(500);
+    INPUT input;
+    ZeroMemory(&input, sizeof(INPUT));
+    input.type = INPUT_KEYBOARD;
+    input.ki.wVk = 0x65;
+    SendInput(1, &input, sizeof(INPUT));
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(INPUT));
+    /*
+    //Structure for the keyboard event
+    INPUT ip;
+
+    //Set up the INPUT structure
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.time = 0;
+    ip.ki.wVk = 0; //We're doing scan codes instead
+    ip.ki.dwExtraInfo = 0;
+
+    //This let's you do a hardware scan instead of a virtual keypress
+    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
+    ip.ki.wScan = 0x65; //Set a unicode character to use (A)
+
+    //Send the press
+    SendInput(1, &ip, sizeof(INPUT));
+
+    //Prepare a keyup event
+    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+    SendInput(1, &ip, sizeof(INPUT));
+    */
+}
+int main(int argc, char const *argv[])
+{
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646309(v=vs.85).aspx
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+    if (RegisterHotKey(NULL, 1, 0, VK_F1) && RegisterHotKey(NULL, 1, 0, VK_F2) && RegisterHotKey(NULL, 1, 0, VK_F3) && RegisterHotKey(NULL, 1, 0, VK_F4) && RegisterHotKey(NULL, 1, 0, 0x51))
+    {
+        std::cout << "Hotkey 'ALT+b' registered, using MOD_NOREPEAT flag\n"
+                  << std::endl;
+    }
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms644958(v=vs.85).aspx
+    MSG msg = {0};
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms644936(v=vs.85).aspx
+    while (GetMessage(&msg, NULL, 0, 0) != 0)
+    {
+        if (msg.message == WM_HOTKEY)
+        {
+            short y = (msg.lParam & 0xffff0000) >> 16;
+            if (y == 112)
+            {
+                Curve();
+            }
+            else if (y == 113)
+            {
+                Curve1();
+            }
+            else if (y == 114)
+            {
+                Curve2();
+            }
+            else if (y == 115)
+            {
+                SaveAndClose();
+            }
+            else if (y == 81)
+            {
+                Camera();
+            }
+            std::cout << "WM_HOTKEY received\n"
+                      << y
+                      << std::endl;
+        }
+    }
+    return 0;
+}
+// g++ p.cpp -o b
